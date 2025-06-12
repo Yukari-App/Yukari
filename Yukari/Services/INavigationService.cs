622f0e583b2e3b9e6b223dc274c83174603b3b0a
TXG0Fk3 <leovertonx12@outlook.com>
2025-06-12 16:35:57 -0300

    Feat: Add Navigation Interface

---
+++ Yukari/Services/INavigationService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace Yukari.Services
+{
+    public interface INavigationService
+    {
+        void Initialize(Frame frame);
+        bool CanGoBack { get; }
+        void Navigate(Type pageType, object parameter = null);
+        bool GoBack();
+    }
+}
