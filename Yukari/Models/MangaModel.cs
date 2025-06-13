948621152c85a4f2efbc09ecc7de8620d1483247
TXG0Fk3 <leovertonx12@outlook.com>
2025-06-13 18:14:05 -0300

    feat: add initial MandaModel implementation

---
+++ Yukari/Models/MangaModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Yukari.Models
+{
+    public class Manga
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string CoverImageUrl { get; set; }
+    }
+
+    public class MangaChapter
+    {
+        public Guid Id { get; set; }
+        public Guid MangaId { get; set; }
+        public string Title { get; set; }
+        public string ChapterUrl { get; set; }
+    }
+
+    public class MangaPage
+    {
+        public Guid Id { get; set; }
+        public Guid ChapterId { get; set; }
+        public int PageNumber { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
