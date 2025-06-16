ba1d39d93e44bcf0c57dbd5919fd2b6ad5498cb9
TXG0Fk3 <leovertonx12@outlook.com>
2025-06-16 18:07:42 -0300

    feat: add FavoritesPageViewModel.cs

---
+++ Yukari/ViewModels/FavoritesPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using Yukari.Models;
+
+namespace Yukari.ViewModels
+{
+    public partial class FavoritesPageViewModel : ObservableObject
+    {
+        [ObservableProperty] private ObservableCollection<Manga> _favoriteMangas = new();
+
+        public FavoritesPageViewModel()
+        {
+
+        }
+    }
+}
