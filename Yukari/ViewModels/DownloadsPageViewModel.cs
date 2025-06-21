350fe4ae15fb76f7b4c2511a8a9e6448b4d0f4b1
TXG0Fk3 <leovertonx12@outlook.com>
2025-06-20 23:55:28 -0300

    feat: add DownloadsPageViewModel.cs

---
+++ Yukari/ViewModels/DownloadsPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Yukari.ViewModels
+{
+    public partial class DownloadsPageViewModel : ObservableObject
+    {
+        [ObservableProperty] private bool _isPaused;
+
+        public string PauseIcon => IsPaused ? "\uE769" : "\uE768";
+
+        public DownloadsPageViewModel()
+        {
+            
+        }
+
+        [RelayCommand]
+        public void TogglePause()
+        {
+            IsPaused = !IsPaused;
+
+            OnPropertyChanged(nameof(PauseIcon));
+        }
+    }
+}
