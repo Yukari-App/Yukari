a495c43f3196581e28df268729b7349c1fecdf71
TXG0Fk3 <leovertonx12@outlook.com>
2025-06-27 18:49:26 -0300

    feat: add ToggleIconConverter

---
+++ Yukari/Helpers/Converters/ToggleIconConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Yukari.Helpers.Converters
+{
+    public partial class ToggleIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isChecked = value as bool? == true;
+            var glyphs = (parameter as string)?.Split(',');
+
+            return new FontIcon { Glyph = isChecked ? glyphs[1].Trim() : glyphs[0].Trim() };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+            throw new NotImplementedException();
+    }
+}
