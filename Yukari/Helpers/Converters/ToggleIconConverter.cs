46e8f22ccf764dcdf9adfc20213274a58a56eb73
TXG0Fk3 <leovertonx12@outlook.com>
2025-06-30 20:14:28 -0300

    feat: add support for icon and text

--- Yukari/Helpers/Converters/ToggleIconConverter.cs
+++ Yukari/Helpers/Converters/ToggleIconConverter.cs
@@ -9,9 +9,25 @@ namespace Yukari.Helpers.Converters
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             bool isChecked = value as bool? == true;
-            var glyphs = (parameter as string)?.Split(',');
+            var parameters = (parameter as string)?.Split(',');
 
-            return new FontIcon { Glyph = isChecked ? glyphs[1].Trim() : glyphs[0].Trim() };
+            if (parameters.Length == 2)
+            {
+                return new FontIcon { Glyph = isChecked ? parameters[1].Trim() : parameters[0].Trim() };
+            }
+            else
+            {
+                var stackPanel = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal,
+                    Spacing = 8
+                };
+
+                stackPanel.Children.Add(new FontIcon { Glyph = isChecked ? parameters[1].Trim() : parameters[0].Trim(), FontSize = 16 });
+                stackPanel.Children.Add(new TextBlock { Text = isChecked ? parameters[3].Trim() : parameters[2].Trim() });
+
+                return stackPanel;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language) =>
