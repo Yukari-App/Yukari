2826a21dd7a8692c54df172361d56c25651c4d70
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-01 16:51:44 -0300

    feat: create ChapterItemViewModel.cs

---
+++ Yukari/ViewModels/ChapterItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Yukari.Models;
+
+namespace Yukari.ViewModels
+{
+    class ChapterItemViewModel : ObservableObject
+    {
+        public ChapterItemViewModel(MangaChapter chapter)
+        {
+
+        }
+    }
+}
