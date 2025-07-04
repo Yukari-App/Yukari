636ed9c0571405ddcb897c5e4adbe8d30b38599a
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-04 18:26:49 -0300

    feat: create IMangaService interface

---
+++ Yukari/Services/IMangaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Yukari.Models;
+
+namespace Yukari.Services
+{
+    internal interface IMangaService
+    {
+        Task<Manga?> GetMangaAsync(Guid id);
+    }
+}
