f9d0a92e9e89b4002ee078812be2a8187b704297
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-04 18:26:30 -0300

    feat: create ISqliteService interface

---
+++ Yukari/Services/ISqliteService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Yukari.Models;
+
+namespace Yukari.Services
+{
+    internal interface ISqliteService
+    {
+        Task<Manga?> GetMangaByIdAsync(Guid id);
+    }
+}
