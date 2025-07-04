d10fbc3d0b0afcbd364eb5fe2d86c453a01f26a6
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-04 20:04:36 -0300

    feat: add IMangaService property

--- Yukari/ViewModels/FavoritesPageViewModel.cs
+++ Yukari/ViewModels/FavoritesPageViewModel.cs
@@ -1,16 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Composition.Interactions;
 using System.Collections.ObjectModel;
 using Yukari.Models;
+using Yukari.Services;
 
 namespace Yukari.ViewModels
 {
     public partial class FavoritesPageViewModel : ObservableObject
     {
+        private IMangaService _mangaService;
+
         [ObservableProperty] private ObservableCollection<Manga> _favoriteMangas = new();
 
-        public FavoritesPageViewModel()
+        public FavoritesPageViewModel(IMangaService mangaService)
         {
-
+            _mangaService = mangaService;
         }
     }
 }
