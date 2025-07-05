04bb2e851ce056b6d8087a9cd5c506f8481e44be
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-04 21:37:35 -0300

    feat: add Year property to Manga

--- Yukari/Models/MangaModel.cs
+++ Yukari/Models/MangaModel.cs
@@ -9,6 +9,7 @@ namespace Yukari.Models
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
+        public int Year { get; set; }
         public string CoverImageUrl { get; set; }
         public bool IsFavorite { get; set; }
     }
