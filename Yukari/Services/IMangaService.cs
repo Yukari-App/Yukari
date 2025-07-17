a1196afa3b8012f640238ea032765a76c4fb5ef9
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-17 17:22:35 -0300

    refactor: consolidate search methods into a single 'get' method with filters

--- Yukari/Services/IMangaService.cs
+++ Yukari/Services/IMangaService.cs
@@ -8,10 +8,9 @@ namespace Yukari.Services
     public interface IMangaService
     {
         Task<Manga?> GetMangaByIdAsync(Guid id);
-        Task<List<Manga>> GetFavoriteMangasAsync();
+        Task<List<Manga>> GetMangasAsync(string? queryText = null);
+        Task<List<Manga>> GetFavoriteMangasAsync(string? queryText = null);
         Task<MangaChapter> GetMangaChapterAsync(Guid chapterId);
         Task<List<MangaChapter>> GetAllMangaChaptersAsync(Guid mangaId);
-        Task<List<Manga>> SearchFavoriteMangasAsync(string? queryText);
-        Task<List<Manga>> SearchMangasAsync(string? queryText);
     }
 }
