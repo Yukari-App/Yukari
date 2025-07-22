7ee5eb90f87874bf18c7fdf157975a0fbeadcabe
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-22 20:24:02 -0300

    feat: implement initial MangaItemViewModel

--- Yukari/ViewModels/MangaItemViewModel.cs
+++ Yukari/ViewModels/MangaItemViewModel.cs
@@ -1,9 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using Yukari.Models;
+using Yukari.Services;
 
 namespace Yukari.ViewModels
 {
     public partial class MangaItemViewModel : ObservableObject
     {
-        public MangaItemViewModel() { }
+        private readonly IMangaService _mangaService;
+
+        private Manga _manga;
+
+        public Guid? Id => _manga?.Id;
+        public string Title => _manga?.Title ?? "Loading...";
+        public string CoverImageUrl => _manga?.CoverImageUrl;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(PinText), nameof(PinIcon))]
+        private bool _isPinned;
+
+        public string PinText => IsPinned ? "Unpin" : "Pin";
+        public string PinIcon => IsPinned ? "\uE77A" : "\uE718";
+        
+        public MangaItemViewModel(Manga manga, IMangaService MangaService)
+        {
+            _manga = manga;
+            _mangaService = MangaService;
+        }
+
+        [RelayCommand]
+        public void TogglePin()
+        {
+            IsPinned = !IsPinned;
+        }
     }
 }
