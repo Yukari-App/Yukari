e58208c3439e85f04ed204b13cd09ebe5014bfe4
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-30 20:56:27 -0300

    feat: add BooleanNegationConverter

---
+++ Yukari/Helpers/Converters/BooleanNegationConverter .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Yukari.Helpers.Converters
+{
+    public class BooleanNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
