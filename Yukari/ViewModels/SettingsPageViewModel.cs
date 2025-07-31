7bf295a064ec625a1655b4a76e1d8760ac2ac307
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-30 21:04:39 -0300

    feat: add initial SettingsPageViewModel

---
+++ Yukari/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Yukari.Models;
+using Yukari.Services;
+
+namespace Yukari.ViewModels
+{
+    public partial class SettingsPageViewModel : ObservableObject
+    {
+        private readonly IMangaService _mangaService;
+
+        [ObservableProperty]
+        private MangaSourceModel _defaultMangaSource;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsMangaSourcesEmpty))]
+        private ObservableCollection<MangaSourceModel> _mangaSources = new();
+
+        public bool IsMangaSourcesEmpty => MangaSources.Count == 0;
+
+        public SettingsPageViewModel(IMangaService mangaService)
+        {
+            _mangaService = mangaService;
+
+            _ = LoadMangaSourcesAsync();
+        }
+
+        private async Task LoadMangaSourcesAsync()
+        {
+            MangaSources = new(await _mangaService.GetMangaSourcesAsync());
+        }
+    }
+}
