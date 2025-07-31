d9b7c77a3007df173fe37236b06547a3d137716c
TXG0Fk3 <leovertonx12@outlook.com>
2025-07-30 21:06:34 -0300

    feat: add initial MangaSourceModel

---
+++ Yukari/Models/MangaSourceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Yukari.Models
+{
+    public class MangaSourceModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+        public string SourceUrl { get; set; }
+        public bool IsEnabled { get; set; } = true;
+    }
+}
