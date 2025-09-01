1c3920164cda33670b08a97ad494f4bd9ff5cb13
TXG0Fk3 <leovertonx12@outlook.com>
2025-09-01 14:56:33 -0300

    feat: initial IMangaApiService implementation

---
+++ Yukari/Services/IMangaApiService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Yukari.Models;
+
+namespace Yukari.Services
+{
+    public interface IMangaApiService
+    {
+        Task<List<Manga>> SearchMangasAsync(string query);
+        Task<List<Manga>> GetTrendingMangasAsync();
+        Task<Manga?> GetMangaByIdAsync(Guid id);
+        Task<MangaChapter> GetMangaChapterAsync(Guid chapterId);
+        Task<List<MangaChapter>> GetAllMangaChaptersAsync(Guid mangaId);
+    }
+}
