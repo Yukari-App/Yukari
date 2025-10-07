1e5903ab0910c8d000977f4836a172502134f0d6
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-07 16:58:25 -0300

    fix: correct the name of ComicId

---
+++ Yukari/Models/ChapterModel.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Yukari.Models
+{
+    internal class ChapterModel
+    {
+        public string Id { get; set; }
+        public string ComicId { get; set; }
+        public string Source { get; set; }
+        public string? Title { get; set; }
+        public int Number { get; set; }
+        public string Volume { get; set; }
+        public string Language { get; set; }
+        public string? Groups { get; set; }
+        public DateOnly LastUpdate { get; set; }
+        public int Pages { get; set; }
+        public int? LastPageRead { get; set; }
+        public bool? IsDownloaded { get; set; }
+        public bool? IsRead { get; set; }
+    }
+}
