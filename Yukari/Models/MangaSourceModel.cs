e72791e6ac5bd333091cb9aebfc66826ef8a2a41
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-07 16:09:59 -0300

    feat: add MangaSourceModel

--- Yukari/Models/MangaSourceModel.cs
+++ Yukari/Models/MangaSourceModel.cs
@@ -1,11 +1,12 @@
-﻿namespace Yukari.Models
+namespace Yukari.Models
 {
     public class MangaSourceModel
     {
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string IconUrl { get; set; }
-        public string SourceUrl { get; set; }
+        public string Version { get; set; }
+        public string? LogoUrl { get; set; }
+        public string? Description { get; set; }
+        public string DllPath { get; set; }
         public bool IsEnabled { get; set; } = true;
     }
 }
