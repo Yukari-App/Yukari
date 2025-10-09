ca0241f35a2b658eeda34362e75e75c68637d17f
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-09 01:54:21 -0300

    refactor: change parameters for some methods

--- Yukari/Services/IComicService.cs
+++ Yukari/Services/IComicService.cs
@@ -7,10 +7,10 @@ namespace Yukari.Services
 {
     internal interface IComicService
     {
-        Task<List<ComicModel>> SearchComicsAsync(string? queryText = null, ComicSourceType sourceType = ComicSourceType.Auto);
-        Task<List<ComicModel>> GetFavoriteComicsAsync(string? queryText = null);
+        Task<List<ComicModel>> SearchComicsAsync(string? queryText, Dictionary<string, List<string>> filters);
+        Task<List<ComicModel>> GetFavoriteComicsAsync(string? queryText, string filter);
         Task<ComicModel?> GetComicDetailsAsync(string id, ComicSourceType sourceType = ComicSourceType.Auto);
-        Task<List<ChapterModel>> GetAllChaptersAsync(string comicId, ComicSourceType sourceType = ComicSourceType.Auto);
+        Task<List<ChapterModel>> GetAllChaptersAsync(string comicId, string language, ComicSourceType sourceType = ComicSourceType.Auto);
         Task<ChapterModel> GetChapterAsync(string chapterId, ComicSourceType sourceType = ComicSourceType.Auto);
         Task<List<ChapterPageModel>> GetChapterPagesAsync(string chapterId, ComicSourceType sourceType = ComicSourceType.Auto);
         Task<List<ComicSourceModel>> GetComicSourcesAsync();
