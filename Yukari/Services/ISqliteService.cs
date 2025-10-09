798d1a8dec7bea3e3612adf9543d7fed2a28c957
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-09 02:02:47 -0300

    refactor: remove methods

--- Yukari/Services/ISqliteService.cs
+++ Yukari/Services/ISqliteService.cs
@@ -6,8 +6,6 @@ namespace Yukari.Services
 {
     internal interface ISqliteService
     {
-        Task<ComicModel?> GetComicByIdAsync(string id);
-        Task<List<ComicModel>> GetComicsAsync(string? queryText = null);
         Task<List<ComicModel>> GetFavoriteComicsAsync(string? queryText = null);
         Task<ChapterModel> GetChapterAsync(string chapterId);
         Task<List<ChapterModel>> GetAllChaptersAsync(string mangaId);
