f6e81283f007ac1ffd64e9a7ca69fdd62fbf8263
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-09 12:22:44 -0300

    refactor: rename methods for clarity

--- Yukari/Services/ISourceService.cs
+++ Yukari/Services/ISourceService.cs
@@ -11,9 +11,9 @@ namespace Yukari.Services
 
         IReadOnlyList<Filter> GetFilters();
         IReadOnlyDictionary<string, string> GetLanguages();
-        Task<IReadOnlyList<ComicModel>> SearchAsync(string query, Dictionary<string, List<string>> filters);
-        Task<IReadOnlyList<ComicModel>> GetTrendingAsync(Dictionary<string, List<string>> filters);
-        Task<ComicModel?> GetDetailsAsync(string mangaId);
+        Task<IReadOnlyList<ComicModel>> SearchComicsAsync(string query, Dictionary<string, List<string>> filters);
+        Task<IReadOnlyList<ComicModel>> GetTrendingComicsAsync(Dictionary<string, List<string>> filters);
+        Task<ComicModel?> GetComicDetailsAsync(string mangaId);
         Task<IReadOnlyList<ChapterModel>> GetAllChaptersAsync(string mangaId, string language);
         Task<IReadOnlyList<ChapterPageModel>> GetChapterPagesAsync(string chapterId);
     }
