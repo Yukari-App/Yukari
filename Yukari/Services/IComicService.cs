81b0469b968eafd89de442184bb41a444330ee09
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-10 03:14:59 -0300

    feat: add sourceName parameter in SearchComicsAsync

--- Yukari/Services/IComicService.cs
+++ Yukari/Services/IComicService.cs
@@ -7,7 +7,7 @@ namespace Yukari.Services
 {
     internal interface IComicService
     {
-        Task<IReadOnlyList<ComicModel>> SearchComicsAsync(string? queryText, Dictionary<string, List<string>> filters);
+        Task<IReadOnlyList<ComicModel>> SearchComicsAsync(string sourceName, string? queryText, Dictionary<string, List<string>> filters);
         Task<IReadOnlyList<ComicModel>> GetFavoriteComicsAsync(string? queryText, string filter);
         Task<ComicModel?> GetComicDetailsAsync(ContentIdentifier comicIdentifier, ComicSourceType sourceType = ComicSourceType.Auto);
         Task<IReadOnlyList<ChapterModel>> GetAllChaptersAsync(ContentIdentifier comicIdentifier, string language, ComicSourceType sourceType = ComicSourceType.Auto);
