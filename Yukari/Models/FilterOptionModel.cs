3d6cdf060927da99200bee7ef43680c6ca8395ef
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-18 16:41:25 -0300

    refactor: remove useless FilterModel and FilterOptionModel

--- Yukari/Models/FilterOptionModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Yukari.Models
-{
-    public class FilterOptionModel
-    {
-        string Key { get; set; }
-        string DisplayName { get; set; }
-        bool IsSelected { get; set; } = false;
-    }
-}
