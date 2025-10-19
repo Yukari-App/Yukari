47baf1545780fcdd38489fa0de66207e6770d701
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-19 13:06:18 -0300

    refactor: ensure immutability

--- Yukari/Messages/FiltersDialogResultMessage.cs
+++ Yukari/Messages/FiltersDialogResultMessage.cs
@@ -2,5 +2,5 @@ using System.Collections.Generic;
 
 namespace Yukari.Messages
 {
-    public record FiltersDialogResultMessage(Dictionary<string, IReadOnlyList<string>> AppliedFilters);
+    public record FiltersDialogResultMessage(IReadOnlyDictionary<string, IReadOnlyList<string>> AppliedFilters);
 }
--- Yukari/Services/UI/IDialogService.cs
+++ Yukari/Services/UI/IDialogService.cs
@@ -8,6 +8,6 @@ namespace Yukari.Services.UI
     public interface IDialogService
     {
         void Initialize(XamlRoot root);
-        Task<Dictionary<string, IReadOnlyList<string>>?> ShowFiltersDialogAsync(IEnumerable<Filter> filters);
+        Task<IReadOnlyDictionary<string, IReadOnlyList<string>>?> ShowFiltersDialogAsync(IEnumerable<Filter> filters);
     }
 }
