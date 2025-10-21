fbaddb3a285dc322b6731e03776a86993e36c27e
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-21 02:15:08 -0300

    feat: add initial implementation for FiltersDialogViewModel

---
+++ Yukari/ViewModels/Dialogs/FiltersDialogViewModel.cs
@@ -0,0 +1,34 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using Yukari.Core.Models;
+using Yukari.ViewModels.Components;
+
+namespace Yukari.ViewModels.Dialogs
+{
+    public partial class FiltersDialogViewModel : ObservableObject
+    {
+        [ObservableProperty] private List<FilterViewModel> _filters;
+
+        public FiltersDialogViewModel(IReadOnlyList<Filter> filters, IReadOnlyDictionary<string, IReadOnlyList<string>> appliedFilters)
+        {
+            Filters = filters.Select(f => {
+                    var fvm = new FilterViewModel(f);
+                    if (appliedFilters.TryGetValue(f.Key, out var selectedValues))
+                        foreach (var option in fvm.Options)
+                            if (selectedValues.Contains(option.Key))
+                                option.IsSelected = true;
+
+                    return fvm;
+                }).ToList();
+        }
+
+        public IReadOnlyDictionary<string, IReadOnlyList<string>> GetAppliedFilters() =>
+            Filters.Where(f => f.Options.Any(o => o.IsSelected)).ToDictionary(
+                f => f.Key,
+                f => f.AllowMultiple
+                    ? (IReadOnlyList<string>)f.Options.Where(o => o.IsSelected).Select(o => o.Key).ToList()
+                    : new List<string> { f.SelectedOptionIfNotAllowMultiple?.Key! }
+            );
+    }
+}
