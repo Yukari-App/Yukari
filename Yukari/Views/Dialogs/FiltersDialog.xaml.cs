89f5bcb6b60ac30dbe0fb3299bc98c190462f62b
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-21 02:24:57 -0300

    feat: add FilterDialog View

---
+++ Yukari/Views/Dialogs/FiltersDialog.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Yukari.Views.Dialogs
+{
+    public sealed partial class FiltersDialog : Page
+    {
+        public FiltersDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
