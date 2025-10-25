09f02b17192f26b88adae6f7c7d2ead631ddc6e0
TXG0Fk3 <leovertonx12@outlook.com>
2025-10-25 16:36:41 -0300

    refactor: make ComicItemViewModel more cleaner, efficient and safer

--- Yukari/ViewModels/Components/ComicItemViewModel.cs
+++ Yukari/ViewModels/Components/ComicItemViewModel.cs
@@ -1,19 +1,12 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Yukari.Models;
-using Yukari.Services.Comics;
 
 namespace Yukari.ViewModels.Components
 {
     public partial class ComicItemViewModel : ObservableObject
     {
-        private readonly IComicService _comicService;
-
-        private ComicModel _comic;
-
-        public ContentIdentifier? Identifier => new(_comic?.Id, _comic.Source);
-        public string Title => _comic?.Title ?? "Unknown Name";
-        public string CoverImageUrl => _comic?.CoverImageUrl;
+        private readonly ComicModel _comic;
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(PinText), nameof(PinIcon))]
@@ -22,11 +15,12 @@ namespace Yukari.ViewModels.Components
         public string PinText => IsPinned ? "Unpin" : "Pin";
         public string PinIcon => IsPinned ? "\uE77A" : "\uE718";
         
-        public ComicItemViewModel(ComicModel comic, IComicService comicService)
-        {
+        public ContentIdentifier? Identifier => new(_comic.Id, _comic.Source);
+        public string Title => _comic.Title;
+        public string? CoverImageUrl => _comic.CoverImageUrl;
+
+        public ComicItemViewModel(ComicModel comic) =>
             _comic = comic;
-            _comicService = comicService;
-        }
 
         [RelayCommand]
         public void TogglePin()
