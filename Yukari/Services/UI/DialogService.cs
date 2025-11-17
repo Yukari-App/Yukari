1cbd3784a2249e879796aa744b5fc68bb19ceff8
TXG0Fk3 <leovertonx12@outlook.com>
2025-11-17 15:06:24 -0300

    feat: implement initial DialogService

---
+++ Yukari/Services/UI/DialogService.cs
@@ -0,0 +1,33 @@
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Yukari.Core.Models;
+using Yukari.ViewModels.Dialogs;
+using Yukari.Views.Dialogs;
+
+namespace Yukari.Services.UI
+{
+    internal class DialogService : IDialogService
+    {
+        private XamlRoot _xamlRoot;
+
+        public void Initialize(XamlRoot root) =>
+            _xamlRoot = root;
+
+        public async Task<IReadOnlyDictionary<string, IReadOnlyList<string>>?> ShowFiltersDialogAsync(
+            IReadOnlyList<Filter> filters, IReadOnlyDictionary<string, IReadOnlyList<string>> appliedFilters)
+        {
+            FiltersDialogViewModel viewModel = new(filters, appliedFilters);
+
+            var dialog = new FiltersDialog(viewModel)
+            {
+                XamlRoot = _xamlRoot
+            };
+
+            await dialog.ShowAsync();
+
+            return viewModel.GetAppliedFilters();
+        }
+    }
+}
