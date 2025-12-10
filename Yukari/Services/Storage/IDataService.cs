701483efef1b7c1102d149b610647bfdc5f6898e
TXG0Fk3 <leovertonx12@outlook.com>
2025-12-10 18:16:33 -0300

    fix(IDataService): correct name for GetChapterUserDataAsync method

--- Yukari/Services/Storage/IDataService.cs
+++ Yukari/Services/Storage/IDataService.cs
@@ -12,7 +12,7 @@ namespace Yukari.Services.Storage
         Task<ComicModel?> GetComicDetailsAsync(ContentKey ComicKey);
         Task<ComicUserData> GetComicUserDataAsync(ContentKey ComicKey);
         Task<IReadOnlyList<ChapterModel>> GetAllChaptersAsync(ContentKey ComicKey, string language);
-        Task<ChapterUserData> GetChapterUserData(ContentKey comicKey, ContentKey chapterKey);
+        Task<ChapterUserData> GetChapterUserDataAsync(ContentKey comicKey, ContentKey chapterKey);
         Task<IReadOnlyList<ChapterPageModel>> GetChapterPagesAsync(ContentKey chapterKey);
         Task<IReadOnlyList<ComicSourceModel>> GetComicSourcesAsync();
         Task<ComicSourceModel?> GetComicSourceDetailsAsync(string sourceName);
