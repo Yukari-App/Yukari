1e4294a50ef21e4c24aaf371abf9e61e3645ee19
TXG0Fk3 <leovertonx12@outlook.com>
2026-01-13 17:54:43 -0300

    feat(viewmodel): allow the user to add ComicSources via SettingsPage

--- Yukari/ViewModels/Pages/SettingsPageViewModel.cs
+++ Yukari/ViewModels/Pages/SettingsPageViewModel.cs
@@ -1,14 +1,17 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Yukari.Models;
 using Yukari.Services.Comics;
+using Yukari.Services.UI;
 
 namespace Yukari.ViewModels.Pages
 {
-    internal partial class SettingsPageViewModel : ObservableObject
+    public partial class SettingsPageViewModel : ObservableObject
     {
         private readonly IComicService _comicService;
+        private readonly IDialogService _dialogService;
 
         [ObservableProperty] public partial ComicSourceModel? DefaultComicSource { get; set; }
 
@@ -18,11 +21,29 @@ namespace Yukari.ViewModels.Pages
 
         public bool IsComicSourcesEmpty => ComicSources.Count == 0;
 
-        public SettingsPageViewModel(IComicService comicService)
+        public SettingsPageViewModel(IComicService comicService, IDialogService dialogService)
         {
             _comicService = comicService;
+            _dialogService = dialogService;
 
             _ = LoadComicSourcesAsync();
+        }
+
+        [RelayCommand]
+        public async Task AddComicSourceAsync()
+        {
+            var pluginPath = await _dialogService.OpenFilePickerAsync(".dll");
+            if (pluginPath == null) return;
+
+            var result = await _comicService.UpsertComicSourceAsync(pluginPath);
+            if (!result.IsSuccess)
+            {
+                // TO-DO: Show error notification
+                return;
+            }
+
+            await LoadComicSourcesAsync();
+            // TO-DO: Show success notification
         }
 
         private async Task LoadComicSourcesAsync()
