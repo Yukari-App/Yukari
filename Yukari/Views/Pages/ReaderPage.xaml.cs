5e172c877fda3d46901808e320115a4e98dace0e
TXG0Fk3 <leovertonx12@outlook.com>
2026-01-14 17:22:31 -0300

    feat(views): implement initial ReaderPage

---
+++ Yukari/Views/Pages/ReaderPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Yukari.Views.Pages
+{
+    public sealed partial class ReaderPage : Page
+    {
+        public ReaderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
