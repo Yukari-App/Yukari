8c924f730ec4d323a7903416438e2902211d435f
TXG0Fk3 <leovertonx12@outlook.com>
2026-01-27 18:58:33 -0300

    feat(ReaderPage): implement initial page visualization with zoom/dragging support

--- Yukari/Views/Pages/ReaderPage.xaml.cs
+++ Yukari/Views/Pages/ReaderPage.xaml.cs
@@ -1,5 +1,9 @@
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
 using Yukari.Models.DTO;
 using Yukari.ViewModels.Pages;
 
@@ -7,6 +11,11 @@ namespace Yukari.Views.Pages
 {
     public sealed partial class ReaderPage : Page
     {
+        private Point _lastMousePosition;
+        private double _startHorizontalOffset;
+        private double _startVerticalOffset;
+        private bool _isDragging = false;
+
         public ReaderPage()
         {
             InitializeComponent();
@@ -22,6 +31,74 @@ namespace Yukari.Views.Pages
             {
                 if (DataContext is ReaderPageViewModel viewModel)
                     await viewModel.InitializeAsync(args.ComicKey, args.ComicTitle, args.ChapterKey, args.SelectedLang);
+            }
+        }
+
+        private void PageScrollViewer_PointerEntered(object sender, PointerRoutedEventArgs e) =>
+            UpdateCursor((sender as ScrollViewer)!);
+
+        private void PageScrollViewer_PointerExited(object sender, PointerRoutedEventArgs e) =>
+            UpdateCursor((sender as ScrollViewer)!);
+
+        private void PageScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e) =>
+            UpdateCursor((sender as ScrollViewer)!);
+
+        private void PageScrollViewer_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            var pageScrollViewer = (sender as ScrollViewer)!;
+            var properties = e.GetCurrentPoint(null).Properties;
+
+            if (properties.IsLeftButtonPressed && pageScrollViewer.ZoomFactor > 1.0f)
+            {
+                _isDragging = true;
+                _lastMousePosition = e.GetCurrentPoint(null).Position;
+                _startHorizontalOffset = pageScrollViewer.HorizontalOffset;
+                _startVerticalOffset = pageScrollViewer.VerticalOffset;
+
+                pageScrollViewer.CapturePointer(e.Pointer);
+
+                UpdateCursor((sender as ScrollViewer)!);
+            }
+        }
+
+        private void PageScrollViewer_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (_isDragging)
+            {
+                var pageScrollViewer = (sender as ScrollViewer)!;
+                var currentPosition = e.GetCurrentPoint(null).Position;
+
+                double deltaX = currentPosition.X - _lastMousePosition.X;
+                double deltaY = currentPosition.Y - _lastMousePosition.Y;
+
+                pageScrollViewer.ChangeView(_startHorizontalOffset - deltaX, _startVerticalOffset - deltaY, null, true);
+            }
+        }
+
+        private void PageScrollViewer_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (_isDragging)
+            {
+                var pageScrollViewer = (sender as ScrollViewer)!;
+
+                _isDragging = false;
+                pageScrollViewer.ReleasePointerCapture(e.Pointer);
+
+                UpdateCursor((sender as ScrollViewer)!);
+            }
+        }
+
+        private void UpdateCursor(ScrollViewer sv)
+        {
+            if (sv.ZoomFactor > 1.0f)
+            {
+                ProtectedCursor = _isDragging
+                    ? InputSystemCursor.Create(InputSystemCursorShape.SizeAll)
+                    : InputSystemCursor.Create(InputSystemCursorShape.Hand);
+            }
+            else
+            {
+                ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Arrow);
             }
         }
     }
