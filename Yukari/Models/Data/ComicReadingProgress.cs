a7328cb758079cfba6d9f64a220e5089ee1df64b
TXG0Fk3 <txg0fk3@proton.me>
2026-03-24 18:55:24 -0700

    feat(Models/Data): add ComicReadingProgress

---
+++ Yukari/Models/Data/ComicReadingProgress.cs
@@ -0,0 +1,8 @@
+﻿namespace Yukari.Models.Data
+{
+    public class ComicReadingProgress
+    {
+        public required string LanguageCode { get; set; }
+        public string? LastChapterId { get; set; }
+    }
+}
