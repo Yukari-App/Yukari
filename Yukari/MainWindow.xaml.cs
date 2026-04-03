b4a53a426b616ae92d5592fff3ce5da371d5c902
TXG0Fk3 <txg0fk3@proton.me>
2026-04-03 17:37:53 -0300

    feat(MainWindow): add message-based fullscreen toggle to MainWindow

--- Yukari/MainWindow.xaml.cs
+++ Yukari/MainWindow.xaml.cs
@@ -1,47 +1,65 @@
 using System;
 using System.Runtime.InteropServices;
+using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 using Windows.Graphics;
+using Yukari.Messages;
 using Yukari.Views.Pages;
 
 namespace Yukari
 {
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : Window, IRecipient<SetFullscreenMessage>
     {
         private const int MinWidth = 656;
         private const int MinHeight = 500;
 
-        private double _scaleFactor;
+        private readonly IMessenger _messenger;
+
+        private readonly double _scaleFactor;
+        private readonly OverlappedPresenter _presenter;
 
         private Frame _rootFrame = new() { Content = new SplashPage() };
 
         public MainWindow()
         {
             InitializeComponent();
+            _messenger = App.GetService<IMessenger>();
 
             ExtendsContentIntoTitleBar = true;
             AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
             AppWindow.SetIcon("Assets/AppIcon.ico");
 
-            OverlappedPresenter presenter = OverlappedPresenter.Create();
-            presenter.PreferredMinimumWidth = MinWidth;
-            presenter.PreferredMinimumHeight = MinHeight;
-            AppWindow.SetPresenter(presenter);
+            _presenter = OverlappedPresenter.Create();
+            _presenter.PreferredMinimumWidth = MinWidth;
+            _presenter.PreferredMinimumHeight = MinHeight;
+            AppWindow.SetPresenter(_presenter);
 
             _scaleFactor = GetScaleFactor();
             AppWindow.Resize(new SizeInt32((int)(1200 * _scaleFactor), (int)(700 * _scaleFactor)));
 
             Content = _rootFrame;
+            _messenger.RegisterAll(this);
         }
+
+        public void Receive(SetFullscreenMessage message) =>
+            SetFullscreenState(message.IsFullscreen);
 
         public void SetMicaBackdrop() => SystemBackdrop = new MicaBackdrop();
 
         public void NavigateToShell() =>
             _rootFrame.Navigate(typeof(ShellPage), null, new DrillInNavigationTransitionInfo());
+
+        private void SetFullscreenState(bool isFullscreen)
+        {
+            if (isFullscreen)
+                AppWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+            else
+                AppWindow.SetPresenter(_presenter);
+        }
 
         [DllImport("user32.dll")]
         private static extern int GetDpiForWindow(IntPtr hwnd);
