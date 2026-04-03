6fd0be622ff04ed3e088f36193bc71b5c2f867e9
TXG0Fk3 <txg0fk3@proton.me>
2026-04-03 19:01:16 -0300

    feat(helpers/converters): add FullScreenToColumnWidthConverter

---
+++ Yukari/Helpers/UI/Converters/FullScreenToColumnWidthConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace Yukari.Helpers.UI.Converters
+{
+    public class FullScreenToColumnWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) =>
+            (bool)value ? new GridLength(0) : new GridLength(144);
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            string language
+        ) => throw new NotImplementedException();
+    }
+}
