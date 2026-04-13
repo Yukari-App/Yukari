311f7bdc541b13e6e77dcbff0c7edf89540905b3
TXG0Fk3 <txg0fk3@proton.me>
2026-04-13 20:38:10 -0300

    feat(Models/Settings): add WindowState

---
+++ Yukari/Models/Settings/WindowState.cs
@@ -0,0 +1,11 @@
+namespace Yukari.Models.Settings
+{
+    public class WindowState
+    {
+        public double Width { get; set; } = 1260;
+        public double Height { get; set; } = 716;
+        public double Left { get; set; } = 100;
+        public double Top { get; set; } = 100;
+        public bool IsMaximized { get; set; } = false;
+    }
+}
