40273b83e688f341dd898558c0fcfbb7c9701b94
TXG0Fk3 <txg0fk3@proton.me>
2026-04-14 14:40:12 -0300

    feat(Models/AppSettings): add ReadingMode and ScalingMode

--- Yukari/Models/Settings/AppSettings.cs
+++ Yukari/Models/Settings/AppSettings.cs
@@ -6,6 +6,9 @@ namespace Yukari.Models.Settings
     {
         public ThemeMode Theme { get; set; } = ThemeMode.System;
 
+        public ReadingMode ReadingMode { get; set; } = ReadingMode.RightToLeft;
+        public ScalingMode ScalingMode { get; set; } = ScalingMode.FitScreen;
+
         public string? DefaultComicSourceName { get; set; }
         public WindowState MainWindowState { get; set; } = new();
     }
