e55d61110c8890d841351517611bc9021a9bfe3b
TXG0Fk3 <txg0fk3@proton.me>
2026-04-16 00:17:57 -0300

    feat(Views): add InitializationErrorPage for startup error handling

---
+++ Yukari/Views/Pages/InitializationErrorPage.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using Yukari.Helpers;
+
+namespace Yukari.Views.Pages
+{
+    public sealed partial class InitializationErrorPage : Page
+    {
+        public InitializationErrorPage()
+        {
+            InitializeComponent();
+
+            VersionTextBlock.Text = $"Version {AppInfoHelper.Version}";
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.Parameter is Exception ex)
+            {
+                ErrorMessageTextBlock.Text = ex.ToString();
+            }
+        }
+    }
+}
