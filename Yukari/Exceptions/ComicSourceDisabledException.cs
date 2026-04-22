18b384ce040d28f29a6bf0a15c6feb35533714d1
TXG0Fk3 <txg0fk3@proton.me>
2026-04-22 20:24:16 -0300

    feat(Exceptions): add ComicSourceDisabledException

---
+++ Yukari/Exceptions/ComicSourceDisabledException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Yukari.Exceptions
+{
+    public class ComicSourceDisabledException : Exception
+    {
+        public ComicSourceDisabledException(string message)
+            : base(message) { }
+    }
+}
