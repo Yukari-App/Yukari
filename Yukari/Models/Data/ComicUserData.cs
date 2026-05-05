1089796dd66e05f0df609f8cee32edf40620ad2d
TXG0Fk3 <txg0fk3@proton.me>
2026-05-05 05:04:54 -0300

    feat(Models/ComicUserData): add Collections property

--- Yukari/Models/Data/ComicUserData.cs
+++ Yukari/Models/Data/ComicUserData.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace Yukari.Models.Data;
 
 public class ComicUserData
 {
     public string? LastSelectedLang { get; set; }
     public bool IsFavorite { get; set; }
+    public IReadOnlyList<string> Collections { get; set; } = [];
 }
