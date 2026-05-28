6a7c9bd38b455e3624cf98316deb5018f7a1a77d
TXG0Fk3 <txg0fk3@proton.me>
2026-05-27 22:52:08 -0300

    feat(ViewModels/ToggleOption): add toggle command and isSelected constructor param

--- Yukari/ViewModels/Components/ToggleOptionViewModel.cs
+++ Yukari/ViewModels/Components/ToggleOptionViewModel.cs
@@ -1,18 +1,38 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Yukari.ViewModels.Components;
 
 public partial class ToggleOptionViewModel : ObservableObject
 {
+    private bool _isInitializing = true;
+
     public string Key { get; }
     public string DisplayName { get; }
+
+    public IRelayCommand<ToggleOptionViewModel>? ToggleCommand { get; }
 
     [ObservableProperty]
     public partial bool IsSelected { get; set; }
 
-    public ToggleOptionViewModel(string key, string? displayName = null)
+    public ToggleOptionViewModel(
+        string key,
+        bool isSelected,
+        string? displayName = null,
+        IRelayCommand<ToggleOptionViewModel>? toggleCommand = null
+    )
     {
         Key = key;
+        IsSelected = isSelected;
         DisplayName = displayName ?? key;
+        ToggleCommand = toggleCommand;
+
+        _isInitializing = false;
+    }
+
+    partial void OnIsSelectedChanged(bool value)
+    {
+        if (!_isInitializing)
+            ToggleCommand?.Execute(this);
     }
 }
