8c4cc9e5f463f756c091a3d2e36330bbf4ba15b3
TXG0Fk3 <txg0fk3@proton.me>
2026-05-29 14:27:30 -0300

    refactor(Helpers): use file-scoped namespace in WindowHelper

--- Yukari/Helpers/WindowHelper.cs
+++ Yukari/Helpers/WindowHelper.cs
@@ -3,25 +3,24 @@ using System.Runtime.InteropServices;
 using Microsoft.UI.Xaml;
 using WinRT.Interop;
 
-namespace Yukari.Helpers
+namespace Yukari.Helpers;
+
+public static class WindowHelper
 {
-    public static class WindowHelper
+    private const int SW_RESTORE = 9;
+
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    public static void BringToFront(Window window)
     {
-        private const int SW_RESTORE = 9;
+        ArgumentNullException.ThrowIfNull(window);
 
-        [DllImport("user32.dll")]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        public static void BringToFront(Window window)
-        {
-            ArgumentNullException.ThrowIfNull(window);
-
-            var hwnd = WindowNative.GetWindowHandle(window);
-            ShowWindow(hwnd, SW_RESTORE);
-            SetForegroundWindow(hwnd);
-        }
+        var hwnd = WindowNative.GetWindowHandle(window);
+        ShowWindow(hwnd, SW_RESTORE);
+        SetForegroundWindow(hwnd);
     }
 }
