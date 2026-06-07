9c749a9caf8c4a6a0e60d32f36faa21144d6aa86
TXG0Fk3 <txg0fk3@proton.me>
2026-06-07 18:26:52 -0300

    feat(DownloadsPage): implement downloads management

--- Yukari/ViewModels/Pages/DownloadsPageViewModel.cs
+++ Yukari/ViewModels/Pages/DownloadsPageViewModel.cs
@@ -1,22 +1,84 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Yukari.Enums;
+using Yukari.Models;
+using Yukari.Services.Storage;
 
 namespace Yukari.ViewModels.Pages;
 
 public partial class DownloadsPageViewModel : ObservableObject
 {
-    [ObservableProperty]
-    public partial bool IsPaused { get; set; }
+    private readonly IDownloadService _downloadService;
 
-    public string PauseIcon => IsPaused ? "\uE769" : "\uE768";
+    public ReadOnlyObservableCollection<DownloadItem> Downloads { get; set; }
 
-    public DownloadsPageViewModel() { }
+    public bool NoDownloads => Downloads.Count == 0;
+
+    public DownloadsPageViewModel(IDownloadService downloadService)
+    {
+        _downloadService = downloadService;
+
+        Downloads = _downloadService.GetAllDownloads();
+        _downloadService.DownloadsCollectionChanged += OnDownloadsCollectionChanged;
+    }
+
+    public void OnNavigatedFrom() =>
+        _downloadService.DownloadsCollectionChanged -= OnDownloadsCollectionChanged;
 
     [RelayCommand]
-    public void TogglePause()
-    {
-        IsPaused = !IsPaused;
+    private void ClearFinishedDownloads() => _downloadService.ClearFinishedDownloads();
 
-        OnPropertyChanged(nameof(PauseIcon));
+    [RelayCommand]
+    private void CancelAllUnfinishedDownloads()
+    {
+        foreach (var item in Downloads)
+        {
+            if (item.Status is DownloadStatus.Downloading or DownloadStatus.Queued)
+                item.Cancel();
+        }
     }
+
+    [RelayCommand]
+    private void OnCancelOrRetryItemClick(DownloadItem item)
+    {
+        switch (item.Status)
+        {
+            case DownloadStatus.Downloading:
+            case DownloadStatus.Queued:
+                CancelDownload(item);
+                break;
+            case DownloadStatus.Failed:
+            case DownloadStatus.Cancelled:
+                RetryDownload(item);
+                break;
+        }
+    }
+
+    [RelayCommand]
+    private async Task OnDeleteItemClickAsync(DownloadItem item) =>
+        await _downloadService.DeleteChapterDownloadAsync(item.ComicKey, item.ChapterKey);
+
+    private void CancelDownload(DownloadItem item)
+    {
+        if (item.Status is DownloadStatus.Downloading or DownloadStatus.Queued)
+            item.Cancel();
+    }
+
+    private void RetryDownload(DownloadItem item)
+    {
+        if (item.Status is DownloadStatus.Failed or DownloadStatus.Cancelled)
+            _downloadService.EnqueueChapterDownload(
+                item.ComicKey,
+                item.ChapterKey,
+                item.ComicTitle,
+                item.ChapterTitle,
+                item.PageProvider
+            );
+    }
+
+    private void OnDownloadsCollectionChanged(object? sender, EventArgs e) =>
+        OnPropertyChanged(nameof(NoDownloads));
 }
--- Yukari/Views/Pages/DownloadsPage.xaml.cs
+++ Yukari/Views/Pages/DownloadsPage.xaml.cs
@@ -1,4 +1,7 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using Yukari.Models;
 using Yukari.ViewModels.Pages;
 
 namespace Yukari.Views.Pages;
@@ -11,7 +14,25 @@ public sealed partial class DownloadsPage : Page
     {
         InitializeComponent();
 
-        ViewModel = new DownloadsPageViewModel();
+        ViewModel = App.GetService<DownloadsPageViewModel>();
         DataContext = ViewModel;
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+        base.OnNavigatedFrom(e);
+        ViewModel.OnNavigatedFrom();
+    }
+
+    private void CancelOrRetryItemButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.CommandParameter is DownloadItem item)
+            ViewModel.CancelOrRetryItemClickCommand.Execute(item);
+    }
+
+    private void DeleteItemButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.CommandParameter is DownloadItem item)
+            ViewModel.DeleteItemClickCommand.Execute(item);
     }
 }
