86ec9a3001e5ea0aab3e3c9c2a1efc947ee28d25
TXG0Fk3 <txg0fk3@proton.me>
2026-06-25 09:06:52 -0300

    feat(Helpers): add ComicSourceExtensions

---
+++ Yukari/Helpers/ComicSourceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Yukari.Helpers;
+
+public static class ComicSourceExtensions
+{
+    public static bool IsCoreOutdated(this string? versionString)
+    {
+        if (string.IsNullOrWhiteSpace(versionString))
+            return false;
+
+        var idx = versionString.IndexOf("+core", StringComparison.OrdinalIgnoreCase);
+        if (idx < 0)
+            return false;
+
+        var verStr = versionString[(idx + "+core".Length)..];
+        if (!Version.TryParse(verStr, out var required))
+            return false;
+
+        var current = new Version(
+            AppInfoHelper.CoreVersion.Major,
+            AppInfoHelper.CoreVersion.Minor,
+            AppInfoHelper.CoreVersion.Build
+        );
+
+        var requiredNormalized = new Version(
+            required.Major,
+            required.Minor,
+            required.Build >= 0 ? required.Build : 0
+        );
+
+        return requiredNormalized < current;
+    }
+}
