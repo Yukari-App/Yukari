587e3f1d462d00760ba972fea6cf38dcd134d0b8
TXG0Fk3 <txg0fk3@proton.me>
2026-07-02 10:53:23 -0300

    fix(Services/Notification): change default method parameters in the interface

--- Yukari/Services/UI/INotificationService.cs
+++ Yukari/Services/UI/INotificationService.cs
@@ -6,10 +6,10 @@ public interface INotificationService
 {
     void Show(string message, string title, NotificationSeverity severity);
 
-    void ShowSuccess(string message, string title = "Success");
-    void ShowInfo(string message, string title = "Info");
-    void ShowError(string message, string title = "Error");
-    void ShowWarning(string message, string title = "Warning");
+    void ShowSuccess(string message, string? title = null);
+    void ShowInfo(string message, string? title = null);
+    void ShowError(string message, string? title = null);
+    void ShowWarning(string message, string? title = null);
 
     void OnShellReady();
 }
